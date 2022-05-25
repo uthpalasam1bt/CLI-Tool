2268d0f4fe7a1563bf23b2a7ab75f353fac6eee3
Uthpala Samarakoon <uthpala.samarakoon@1billiontech.com>
2022-05-25 15:05:30 +0530

    changed text

--- cli/index.js
+++ cli/index.js
@@ -1,53 +1,55 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const readline = require('readline');
-const path = require('path');
-const figlet = require('figlet');
-const chalk = require('chalk');
-const inquirer = require('inquirer');
-const { copyStepTemplate } = require('./stepTemplate');
-const { editExistingWorkflow } = require('./editWorkflow');
-const { addWorkflow } = require('./addWorkflow');
+const fs = require("fs");
+const readline = require("readline");
+const path = require("path");
+const figlet = require("figlet");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const { copyStepTemplate } = require("./stepTemplate");
+const { editExistingWorkflow } = require("./editWorkflow");
+const { addWorkflow } = require("./addWorkflow");
 
 const OPTIONS = {
-    CREATE_WORKFLOW: 'Create a new workflow',
-    EDIT_WORKFLOW: 'Edit an exisitig workflow'
+  CREATE_WORKFLOW: "Create a new workflow",
+  EDIT_WORKFLOW: "Edit an exisitig workflow",
 };
 
-console.log(chalk.gray(figlet.textSync('Nav Guide CLI Tool', { horizontalLayout: 'full' })));
+console.log(
+  chalk.gray(figlet.textSync("VENUS CLI TOOL", { horizontalLayout: "full" }))
+);
 
 const cliOptions = () => {
-    const questions = [
-        {
-            type: 'list',
-            name: 'options',
-            message: 'select an option :',
-            choices: [OPTIONS.CREATE_WORKFLOW, OPTIONS.EDIT_WORKFLOW],
-            default: OPTIONS.CREATE_WORKFLOW
-        }
-    ];
-    return inquirer.prompt(questions);
+  const questions = [
+    {
+      type: "list",
+      name: "options",
+      message: "select an option :",
+      choices: [OPTIONS.CREATE_WORKFLOW, OPTIONS.EDIT_WORKFLOW],
+      default: OPTIONS.CREATE_WORKFLOW,
+    },
+  ];
+  return inquirer.prompt(questions);
 };
 
 const run = async () => {
-    try {
-        const answer = await cliOptions();
+  try {
+    const answer = await cliOptions();
 
-        if (answer && answer.options) {
-            switch (answer.options) {
-                case OPTIONS.CREATE_WORKFLOW:
-                    addWorkflow()
-                    break;
-                case OPTIONS.EDIT_WORKFLOW:
-                    editExistingWorkflow()
-                    break;
-                default:
-                    break;
-            }
-        }
-    } catch (error) {
-        console.log('error', error);
+    if (answer && answer.options) {
+      switch (answer.options) {
+        case OPTIONS.CREATE_WORKFLOW:
+          addWorkflow();
+          break;
+        case OPTIONS.EDIT_WORKFLOW:
+          editExistingWorkflow();
+          break;
+        default:
+          break;
+      }
     }
+  } catch (error) {
+    console.log("error", error);
+  }
 };
 
 run();
